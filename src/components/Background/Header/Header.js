a13f6f905982196125661d1b447d904d292708a8
Kavin Arasu <kavinarasu04@gmail.com>
2024-10-30 20:06:14 -0400

    Homepage Complete

--- src/components/Background/Header/Header.js
+++ src/components/Background/Header/Header.js
@@ -8,37 +8,36 @@ function Header() {
     return (
         <>
             <div className="description">
-                <div className="head">
-                    <h1>Kavin Arasu</h1>
-                    <div className="container">
-                        <p>I'm a&nbsp;</p>
-                        <Typewriter 
-                            className="type" 
-                            options={{
-                                autoStart: true,
-                                loop: true,
-                                deleteSpeed: 20,
-                                strings: ["Cybersecurity", "Front End", "Back End", "Full Stack"]
-                            }} 
-                        />
-                        engineer
-                    
-                    
-                    </div>
-                    <img className="coder" src={coder} alt="coding man" />
-                </div>
+
+                <h1>Kavin Arasu</h1>
+                <div className="container">
+                    <p>I'm a&nbsp;</p>
+                    <Typewriter 
+                        className="type" 
+                        options={{
+                            autoStart: true,
+                            loop: true,
+                            deleteSpeed: 20,
+                            strings: ["Cybersecurity", "Front End", "Back End", "Full Stack"]
+                        }} 
+                    />
+                    engineer
                 
+                
+                </div>
+                <img className="coder" src={coder} alt="coding man" />
 
-
-                    
                 <div className="buttons">
                     <a href={resume} target="_blank" rel="noopener noreferrer">
                         <button className="btn">Resume</button>
                     </a>
                     <a href="#projects" className="btn">Projects</a>
                 </div>
-                
+
             </div>
+                    
+            
+            
 
         </>
     );
